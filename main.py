70b8bae5d9dd8bd71201a7d3ef20a99099ee87b7
PowerfullUser <139379401+PowerfullUser@users.noreply.github.com>
2025-10-18 17:03:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-# File: main.py
-# Purpose: Main application logic for PowerText – a feature-rich text editor
-
-# Copyright 2025 Toshan
-
+# PowerText.py - A multi-tabbed, rich-text editor built with PyQt6.
+#
+# Copyright 2025 Toshan Chowdhury
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Libraries required to be imported
 from PyQt6.QtWidgets import *
@@ -517,4 +522,5 @@ window = PowerText()
 window.show()
 window.setGeometry(100, 100, 1000, 600)
 app.exec()
+
 
